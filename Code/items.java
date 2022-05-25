45122e47c303c5b8efe915abfeb3d4b5e076dd66
KingNelx <tapiajonel606@gmail.com>
2022-05-25 08:54:22 +0800

    some changes

--- Code/items.java
+++ Code/items.java
@@ -3,13 +3,25 @@ package Code;
 public class items {
 
     public void Drinks(){
-        System.out.println(" Available Drinks ");
-        System.out.println(" Coke   ₱25pesos ");
-        System.out.println(" Sprite ₱25pesos ");
+        System.out.println("      ---------------- ");
+        System.out.println("      Available Drinks ");
+        System.out.println("      ---------------- ");
+        System.out.println("  Drinks             Price");
+        System.out.println(" [Coke       ||     ₱15pesos] ");
+        System.out.println(" [Sprite     ||     ₱20pesos] ");
+        System.out.println(" [Royal      ||     ₱25pesos] ");
+        System.out.println(" [Water      ||     ₱30pesos] ");
     }
 
     public void Burgers(){
-
+        System.out.println("      ---------------- ");
+        System.out.println("      Available Burgers ");
+        System.out.println("      ---------------- ");
+        System.out.println("  Drinks                     Price");
+        System.out.println(" [Regular Burger     ||    ₱17pesos] ");
+        System.out.println(" [Cheese Burger      ||    ₱25pesos] ");
+        System.out.println(" [Overload Burger    ||    ₱40pesos] ");
+        System.out.println(" [Chicken Burger     ||    ₱50pesos] ");
     }
 
     public void Pizza(){
--- Code/main.java
+++ Code/main.java
@@ -7,16 +7,18 @@ public class main {
     public static void main (String[]args){
         Scanner input = new Scanner(System.in);
         ArrayList<String>s1 = new ArrayList<>();
+        items s2 = new items();
+//
+//        System.out.println(" What is you're order? ");
+//        String order = input.nextLine();
+//
+//        System.out.println(" Quantity ? ");
+//        int quantity = input.nextInt();
+//
+//        s1.add(order);
 
-        System.out.println(" What is you're order? ");
-        String order = input.nextLine();
-
-        System.out.println(" Quantity ? ");
-        int quantity = input.nextInt();
-
-        s1.add(order);
-
-
+        s2.Drinks();
+    s2.Burgers();
 
     }
 }
