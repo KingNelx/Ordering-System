70bd0fd84a22ac314af897718140ca620d32821e
KingNelx <tapiajonel606@gmail.com>
2022-05-26 06:16:02 +0800

    some changes

--- Code/main.java
+++ Code/main.java
@@ -1,21 +1,7 @@
 package Code;
 
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class main {
-    public static void main (String[]args){
-        Scanner input = new Scanner(System.in);
-        ArrayList<String>s1 = new ArrayList<>();
-        items s2 = new items();
-
-        System.out.println(" What is your order? ");
-        String order = input.nextLine();
-
-        s1.add(order);
-
-        System.out.println(" How many?");
-        int count = input.nextInt();
-
+    public static void main (String[]args) {
     }
 }
--- Code/payment.java
+++ Code/payment.java
@@ -2,4 +2,14 @@ package Code;
 
 public class payment {
 
+    public void checkPayment(int balance, int cost){
+        if(cost > balance){
+            System.out.println(" Im sorry, it seem that your balance is not enough. ");
+            System.out.println(" Total Balance: " + balance);
+        }else{
+            balance = balance - cost;
+            System.out.println(" Successfully purchased ");
+            System.out.println();
+        }
+    }
 }
