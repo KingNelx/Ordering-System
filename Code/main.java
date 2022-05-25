10e77921ae04b2c8b730284193a6855d6bb68f39
KingNelx <tapiajonel606@gmail.com>
2022-05-25 09:02:43 +0800

    some changes

--- Code/main.java
+++ Code/main.java
@@ -8,17 +8,27 @@ public class main {
         Scanner input = new Scanner(System.in);
         ArrayList<String>s1 = new ArrayList<>();
         items s2 = new items();
-//
-//        System.out.println(" What is you're order? ");
-//        String order = input.nextLine();
-//
-//        System.out.println(" Quantity ? ");
-//        int quantity = input.nextInt();
-//
-//        s1.add(order);
 
-        s2.Drinks();
-    s2.Burgers();
+        int balance = 500;
+        int total = 0;
+
+        System.out.println(" What is you're order? ");
+        String order = input.nextLine();
+
+        System.out.println(" Quantity ? ");
+        int quantity = input.nextInt();
+
+        s1.add(order);
+
+        if(s1.contains("Burger")){
+            System.out.println(" Your Order is: " + " " + quantity + " " + order);
+            total = total * quantity;
+        }else if (s1.contains("Cheese")){
+            System.out.println(" Your Order is: " + " " + quantity + " " + order);
+        }
+
+
+
 
     }
 }
