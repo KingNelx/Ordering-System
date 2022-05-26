82af83f6786f727f0f87858cc869164a1822eb39
KingNelx <tapiajonel606@gmail.com>
2022-05-26 17:47:02 +0800

    some changes

---
+++ Code/Alerts.java
@@ -0,0 +1,13 @@
+package Code;
+
+public class Alerts {
+
+    public String error(){
+        return " Oh it seems that your password was not match ";
+    }
+
+    public String success(){
+        return " Password match ";
+    }
+
+}
--- Code/CreateAccount.java
+++ Code/CreateAccount.java
@@ -1,25 +1,35 @@
 package Code;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CreateAccount {
 
     Scanner input = new Scanner(System.in);
+    Alerts alert = new Alerts();
 
     private String userName;
     private String password;
+    private String rePassword;
 
     public void createAccount(){
-
         System.out.println(" Create Account ");
-
 
         System.out.print(" Enter username: ");
         userName = input.nextLine();
-
         System.out.print(" Enter password: ");
         password = input.nextLine();
+        System.out.print(" Re enter password: ");
+        rePassword = input.nextLine();
 
-        System.out.println(" Username: " + userName);
-        System.out.println(" Password: " + password);
+        if(Objects.equals(password, rePassword)){
+            String success = alert.success();
+            System.out.println(success);
+        }else{
+            String fail = alert.error();
+            System.out.println(fail);
+        }
+
     }
+
+
 }
