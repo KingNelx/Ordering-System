080a0feeafcfa969542719414a7e7947c79076f3
KingNelx <tapiajonel606@gmail.com>
2022-05-29 08:15:24 +0800

    some changes

--- Code/Wallet.java
+++ Code/Wallet.java
@@ -1,7 +1,12 @@
 package Code;
+import java.util.Random;
 
 public class Wallet {
     public void wallet(){
+        System.out.println(" Check Balance ");
+    }
 
+    public int Balance(){
+        return 1;
     }
 }
