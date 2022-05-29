351570d1630a9aae57d55cf20c5fb2aabf7ade02
KingNelx <tapiajonel606@gmail.com>
2022-05-30 07:46:31 +0800

    update

--- Code/Wallet.java
+++ Code/Wallet.java
@@ -3,10 +3,14 @@ import java.util.Random;
 
 public class Wallet {
     public void wallet(){
-        System.out.println(" Check Balance ");
+        System.out.println(" Balance: " + Balance() +"Php");
     }
 
     public int Balance(){
-        return 1;
+        int []randomBalance = {200, 400, 600, 700, 800, 1000};
+        Random random = new Random();
+        int myBalance = random.nextInt(randomBalance.length);
+        return randomBalance[myBalance];
     }
+
 }
