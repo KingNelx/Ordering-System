757b43f2a530500bf5a87a0f5b87d054c188c6eb
KingNelx <tapiajonel606@gmail.com>
2022-05-30 08:58:14 +0800

    update

--- Code/Options.java
+++ Code/Options.java
@@ -3,6 +3,8 @@ package Code;
 public class Options {
     public void options(){
         System.out.println(" 1. Create Account ");
-        System.out.println(" 2. Log in");
+        System.out.println(" 2. Log in as User ");
+        System.out.println(" 3. Log in as an Admin ");
+        System.out.println(" 4. Exit ");
     }
 }
