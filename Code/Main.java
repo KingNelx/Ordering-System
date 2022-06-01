6e580dd6262acb376c13ca98dc115bd30ce32b84
KingNelx <tapiajonel606@gmail.com>
2022-06-01 23:05:57 +0800

    some changes

--- Code/Main.java
+++ Code/Main.java
@@ -8,18 +8,12 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Options opt = new Options();
 
-       do{
-        opt.options();
-        System.out.print(" Select option: ");
-        choice = input.nextInt();
-        opt.check(choice);
-        }while(choice !=4);
 
-//        while(choice != 4){
-//            opt.options();
-//            System.out.print(" Select option: ");
-//            choice = input.nextInt();
-//            opt.check();
-//        }
+        while(choice != 4){
+            opt.options();
+            System.out.print(" Select option: ");
+            choice = input.nextInt();
+            opt.check(choice);
+        }
     }
 }
--- Code/User.java
+++ Code/User.java
@@ -1,7 +1,20 @@
 package Code;
+import java.util.Scanner;
 
 public class User {
+    Scanner input = new Scanner(System.in);
+    int test;
     public void user(){
-        System.out.println(" User ");
+        System.out.println(" Enter test ");
+        test = input.nextInt();
+
+        if(test == 1){
+            test1();
+        }
+    }
+
+    public void test1(){
+        System.out.println(" Test ");
+        System.exit(0);
     }
 }
