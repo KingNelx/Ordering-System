db304425b9613524340934d3e130b0b3295a15c3
KingNelx <tapiajonel606@gmail.com>
2022-06-02 00:19:09 +0800

    some changes

--- Code/CheckBalance.java
+++ Code/CheckBalance.java
@@ -1,7 +1,15 @@
 package Code;
+import java.util.Random;
 
 public class CheckBalance {
     public boolean isBalanceEnough(int balance, int cost){
         return balance > cost;
+    }
+
+    public int getMoney(){
+        int[]balance = {300, 500, 600, 700, 800, 1000};
+        Random random = new Random();
+        int myMoney = random.nextInt(balance.length);
+        return balance[myMoney];
     }
 }
