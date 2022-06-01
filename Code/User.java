d3b442a56368a6c0c9eafe3a7ac31c761ce27ee0
KingNelx <tapiajonel606@gmail.com>
2022-06-01 23:27:28 +0800

    some changes

--- Code/User.java
+++ Code/User.java
@@ -4,12 +4,23 @@ import java.util.Scanner;
 
 public class User {
     Scanner input = new Scanner(System.in);
+    Scanner drink = new Scanner(System.in);
+    Scanner burger = new Scanner(System.in);
+    Scanner pizza = new Scanner(System.in);
+    Scanner fries = new Scanner(System.in);
+    Scanner dessert = new Scanner(System.in);
+
     UserOption option = new UserOption();
-
-
     Foods food = new Foods();
 //    int test; for testing
-    int choice;
+    private int choice;
+    private int drinkOrder;
+    private int burgerOrder;
+    private int pizzaOrder;
+    private int friesOrder;
+    private int dessertOrder;
+
+    private int order;
     public void user(){
         option.userOpt();
         System.out.print(" Select option: ");
@@ -17,6 +28,17 @@ public class User {
 
         if(choice == 1){
             food.foods();
+            System.out.print(" Select what to order: ");
+            order = input.nextInt();
+
+            switch (order){
+                case 1 ->{
+                    food.Drinks();
+                    System.exit(0);
+                }
+            }
+
+
         }
     }
 
